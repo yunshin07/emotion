353cf97ed8ff463830355b2f1d5ba74daacc6c18
JinPark <jinchul112@gmail.com>
2023-05-24 13:08:12 +0200

    comments align

--- src/components/SelectedView.tsx
+++ src/components/SelectedView.tsx
@@ -131,154 +131,158 @@ export const SelectedView: FC<SelectedViewProps> = ({ selected, topic }) => {
         alignItems: 'center',
         marginBottom: '2rem',
       }}>
-      <Stack
-        alignItems={'center'}
-        justifyContent={'center'}>
+      <Stack alignItems={'center'}>
         <Stack direction={'row'}>
-          {selected.length > 0 && !combo ? (
-            <Stack
-              direction={'row'}
-              spacing={1}>
-              <Paper
-                sx={{
-                  padding: isSmall ? '0.2rem' : '0.5rem',
-                  pl: '0.7rem',
-                  pr: '0.7rem',
-                  borderRadius: '2rem',
-                }}
-                elevation={4}>
-                <Box
-                  width={isSmall ? 30 : 40}
-                  height={isSmall ? 30 : 40}
-                  component={'img'}
-                  src={`assets/htmls/images/${
-                    iconData[topic].icons[selected[0]]?.desc
-                  }.png`}
-                  alt='icon'
-                />
-              </Paper>
-              <Paper
-                sx={{
-                  padding: isSmall ? '0.4rem' : '2rem',
-                  borderRadius: '2rem',
-                }}
-                elevation={4}>
-                {isSmall && (
-                  <Box
-                    component={'div'}
-                    width={30}
-                    height={30}
+          <Stack
+            alignItems={'center'}
+            justifyContent={'center'}>
+            <Stack direction={'row'}>
+              {selected.length > 0 && !combo ? (
+                <Stack
+                  direction={'row'}
+                  spacing={1}>
+                  <Paper
+                    sx={{
+                      padding: isSmall ? '0.2rem' : '0.5rem',
+                      pl: '0.7rem',
+                      pr: '0.7rem',
+                      borderRadius: '2rem',
+                    }}
+                    elevation={4}>
+                    <Box
+                      width={isSmall ? 30 : 40}
+                      height={isSmall ? 30 : 40}
+                      component={'img'}
+                      src={`assets/htmls/images/${
+                        iconData[topic].icons[selected[0]]?.desc
+                      }.png`}
+                      alt='icon'
+                    />
+                  </Paper>
+                  <Paper
+                    sx={{
+                      padding: isSmall ? '0.4rem' : '1.9rem',
+                      borderRadius: '2rem',
+                    }}
+                    elevation={4}>
+                    {isSmall && (
+                      <Box
+                        component={'div'}
+                        width={30}
+                        height={30}
+                      />
+                    )}
+                  </Paper>
+                </Stack>
+              ) : combo ? (
+                <Paper
+                  sx={{
+                    padding: '0.7rem',
+                    pl: '0.7rem',
+                    pr: '0.7rem',
+                    borderRadius: '2rem',
+                  }}
+                  elevation={4}>
+                  <Stack
+                    direction={'row'}
+                    spacing={1}>
+                    <Box
+                      component={'img'}
+                      src={`${BASE_URL}/assets/htmls/images/${
+                        iconData[topic].icons[selected[0]]?.desc
+                      }.png`}
+                      width={isSmall ? 30 : 40}
+                      height={isSmall ? 30 : 40}
+                      alt='icon'
+                    />
+                    <Box
+                      component={'img'}
+                      src={`${BASE_URL}/assets/htmls/images/${
+                        iconData[topic].icons[selected[1]]?.desc
+                      }.png`}
+                      width={isSmall ? 30 : 40}
+                      height={isSmall ? 30 : 40}
+                      alt='icon'
+                    />
+                  </Stack>
+                </Paper>
+              ) : (
+                <Stack
+                  direction={'row'}
+                  spacing={1}>
+                  <Paper
+                    sx={{
+                      padding: isSmall ? '1rem' : '1.9rem',
+                      borderRadius: '2rem',
+                    }}
+                    elevation={4}
                   />
-                )}
-              </Paper>
+                  <Paper
+                    sx={{
+                      padding: isSmall ? '1rem' : '1.9rem',
+                      borderRadius: '2rem',
+                    }}
+                    elevation={4}
+                  />
+                </Stack>
+              )}
             </Stack>
-          ) : combo ? (
-            <Paper
-              sx={{
-                padding: '0.5rem',
-                pl: '0.7rem',
-                pr: '0.7rem',
-                borderRadius: '2rem',
-              }}
-              elevation={4}>
+          </Stack>
+          <Stack
+            direction={'row'}
+            alignItems={'flex-start'}
+            spacing={1}
+            ml={1}>
+            <Stack alignItems={'center'}>
               <Stack
                 direction={'row'}
-                spacing={1}>
-                <Box
-                  component={'img'}
-                  src={`${BASE_URL}/assets/htmls/images/${
-                    iconData[topic].icons[selected[0]]?.desc
+                alignItems={'center'}>
+                <SelectButton
+                  source={`${BASE_URL}/assets/htmls/images/${
+                    !flag.recommend ? 'move01' : 'move01_on'
                   }.png`}
-                  width={isSmall ? 30 : 40}
-                  height={isSmall ? 30 : 40}
-                  alt='icon'
+                  sx={{
+                    cursor: 'pointer',
+                    '&:hover': {
+                      backgroundColor: 'transparent',
+                    },
+                  }}
+                  width={isSmall ? 55 : 75}
+                  height={isSmall ? 55 : 75}
+                  role='option'
+                  onSelect={() => {
+                    handleOptionSelect('recommend');
+                  }}
                 />
-                <Box
-                  component={'img'}
-                  src={`${BASE_URL}/assets/htmls/images/${
-                    iconData[topic].icons[selected[1]]?.desc
+                <SelectButton
+                  source={`${BASE_URL}/assets/htmls/images/${
+                    !flag.next ? 'move02' : 'move02_on'
                   }.png`}
-                  width={isSmall ? 30 : 40}
-                  height={isSmall ? 30 : 40}
-                  alt='icon'
+                  width={isSmall ? 55 : 75}
+                  height={isSmall ? 55 : 75}
+                  sx={{
+                    cursor: 'pointer',
+                    '&:hover': {
+                      backgroundColor: 'transparent',
+                    },
+                  }}
+                  role='option'
+                  onSelect={() => {
+                    handleOptionSelect('next');
+                  }}
                 />
               </Stack>
-            </Paper>
-          ) : (
-            <Stack
-              direction={'row'}
-              spacing={1}>
-              <Paper
-                sx={{
-                  padding: isSmall ? '1rem' : '2rem',
-                  borderRadius: '2rem',
-                }}
-                elevation={4}
-              />
-              <Paper
-                sx={{
-                  padding: isSmall ? '1rem' : '2rem',
-                  borderRadius: '2rem',
-                }}
-                elevation={4}
-              />
             </Stack>
-          )}
+          </Stack>
         </Stack>
         <Typography
           sx={{
             marginTop: '0.5rem',
           }}
           variant='body2'>
-          {handleDescription() || ''}
+          {handleDescription()}&emsp;
+          {handleDesc()}&emsp;
         </Typography>
-      </Stack>
-      <Stack
-        direction={'row'}
-        alignItems={'flex-start'}
-        spacing={1}
-        ml={1}>
-        <Stack alignItems={'center'}>
-          <Stack
-            direction={'row'}
-            alignItems={'center'}>
-            <SelectButton
-              source={`${BASE_URL}/assets/htmls/images/${
-                !flag.recommend ? 'move01' : 'move01_on'
-              }.png`}
-              sx={{
-                cursor: 'pointer',
-                '&:hover': {
-                  backgroundColor: 'transparent',
-                },
-              }}
-              width={isSmall ? 55 : 75}
-              height={isSmall ? 55 : 75}
-              role='option'
-              onSelect={() => {
-                handleOptionSelect('recommend');
-              }}
-            />
-            <SelectButton
-              source={`${BASE_URL}/assets/htmls/images/${
-                !flag.next ? 'move02' : 'move02_on'
-              }.png`}
-              width={isSmall ? 55 : 75}
-              height={isSmall ? 55 : 75}
-              sx={{
-                cursor: 'pointer',
-                '&:hover': {
-                  backgroundColor: 'transparent',
-                },
-              }}
-              role='option'
-              onSelect={() => {
-                handleOptionSelect('next');
-              }}
-            />
-          </Stack>
-          <Typography variant='body2'>{handleDesc()}&emsp;</Typography>
-        </Stack>
       </Stack>
     </Container>
   );
